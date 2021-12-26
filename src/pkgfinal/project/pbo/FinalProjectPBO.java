21964fdc8ff425e9ae4cdadea5b01d02629dc3d9
saviramarsha <96687356+saviramarsha@users.noreply.github.com>
2021-12-26 19:54:58 +0700

    Add files via upload

---
+++ src/pkgfinal/project/pbo/FinalProjectPBO.java
@@ -0,0 +1,33 @@
+package pkgfinal.project.pbo;
+
+import database.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import koperasi.*;
+
+public class FinalProjectPBO extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("formNasabah.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+//        dbHelper db = new dbHelper();
+//        db.createTable();
+//        rekeningDataModel datamodel = new rekeningDataModel();
+//        Rekening rek = new Rekening(1,1);
+//        Perusahaan nasabahIndividu = new Perusahaan(1,"Adiwijaya","Gisting","120",rek);
+//        datamodel.addNasabah(nasabahIndividu);
+    }
+    
+}
